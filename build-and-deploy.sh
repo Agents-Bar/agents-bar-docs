2cba7d9f0013c2cd8ec6f49145045302222ed7c3
Dawid Laszuk <laszukdawid@gmail.com>
2021-04-06 16:04:07 -0700

    Docker service name update and make script as executable

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -5,4 +5,4 @@ export DOMAIN=docs.agents.bar
 docker-compose build
 
 # Deploy part
-docker stack deploy -c docker-compose.yml --with-registry-auth agent-bar-docs
+docker stack deploy -c docker-compose.yml --with-registry-auth agents-bar-docs
